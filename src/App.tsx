eb816eaca9434806530c5fc5f6185b4cc3c8993f
Saka Manish <sakamanish8@gmail.com>
2025-03-12 10:44:30 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -112,7 +112,7 @@ function App() {
     },
     {
       title: "Sai Multiservices",
-      url: "https://saimultiservices.onrendor.com",
+      url: "https://saimultiservices.onrender.com",
       icon: <Globe size={24} />,
       description: "One place for all documents.",
       image: "https://images.unsplash.com/photo-1460925895917-afdab827c52f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2015&q=80"
